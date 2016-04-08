8abff03a87aeb3cf27419170e8dd2675cd61c8ef
kaleemullah360 <kaleemullah360@live.com>
2016-04-08 11:25:49 +0500

    coap & mqtt test methods

---
+++ routes/datacoap.js
@@ -0,0 +1,25 @@
+const StringDecoder = require('string_decoder').StringDecoder;
+const decoder = new StringDecoder('utf8');
+var express = require('express');
+var router = express.Router();
+
+/*
+var URL = require('url')
+var opts = URL.parse('coap://[aaaa::c30c:0:0:2]:5683/test/push')
+opts.observe = true
+
+const coap  = require('coap') // or coap
+    , req   = coap.request(opts);
+
+req.on('response', function(res) {
+  res.pipe(process.stdout);
+  console.log(decoder.write(res.payload));
+})
+req.end();
+*/
+/* GET home page. */
+router.get('/', function(req, resp, next) {
+  resp.send("This is view");
+});
+
+module.exports = router;
