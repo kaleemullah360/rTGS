0a0a3dc5cc6ae83550c52a5e4a5ef2f14383bb12
kaleemullah360 <kaleemullah360@live.com>
2016-04-08 11:22:31 +0500

    initial commit

---
+++ database/rtgs-table.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Localhost-ThisPC
+Source Server Version : 50628
+Source Host           : localhost:3306
+Source Database       : rtgs
+
+Target Server Type    : MYSQL
+Target Server Version : 50628
+File Encoding         : 65001
+
+Date: 2016-04-07 22:28:19
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for rtgs-table
+-- ----------------------------
+DROP TABLE IF EXISTS `rtgs-table`;
+CREATE TABLE `rtgs-table` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `MessageID` int(255) DEFAULT NULL,
+  `UpTime` int(255) DEFAULT NULL,
+  `ClockTime` varchar(255) DEFAULT NULL,
+  `Temperature` varchar(255) DEFAULT NULL,
+  `Battery` varchar(255) DEFAULT NULL,
+  `Protocol` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
