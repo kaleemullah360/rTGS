18823a1ab246c694e7c431a993330e2abb414c0f
kaleemullah360 <kaleemullah360@live.com>
2016-07-26 10:59:33 +0500

    updates

--- routes/rtgs-http.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('rtgs-http', { title: 'HTTP rTGS' });
-});
-
-module.exports = router;
